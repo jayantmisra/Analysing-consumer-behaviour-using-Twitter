726c471408098d672569613f14cf8eb5f348c49e
cmcdxwell <100871652+cmcdxwell@users.noreply.github.com>
2022-05-14 02:42:55 +0100

    Add files via upload

--- plotting_points.py
+++ plotting_points.py
@@ -51,9 +51,20 @@ def scatter_plot(map_fig, tweets):
     map_fig.add_layer(scatter_layer)
 
 
-def geojson_layer(map_figure, tweets, countries_geojson):
-    #with open('world-administrative-boundaries.geojson') as f:
-     #   geometry = json.load(f)
+def countries_syntax(countries):
+    changes = {'United States of America': 'United States', 'United Republic of Tanzania': 'Tanzania',
+               'Guinea Bissau': 'Guinea-Bissau', 'The Gambia': 'Gambia', 'Ivory Coast': 'CÃ´te d\'Ivoire',
+               'Republic of Congo': 'Liberia', 'Macedonia': 'North Macedonia', 'Republic of Serbia': 'Serbia',
+               'Czech Republic': 'Czechia'}
+
+    for i in changes:
+        if changes[i] in countries:
+            countries[i] = countries.pop(changes[i])
+
+    return countries
+
+
+def country_sentiment(tweets):
 
     countries = {}
     countries_count = {}
@@ -64,11 +75,17 @@ def geojson_layer(map_figure, tweets, countries_geojson):
         else:
             countries[tweets['Country'][i]] = tweets['compound'][i]
             countries_count[tweets['Country'][i]] = 1
+
     for x in countries_count:
-        countries[x] = countries[x]/countries_count[x]
+        if countries_count[x] != 0:
+            countries[x] = countries[x] / countries_count[x]
 
-    countries['United States of America'] = countries.pop('United States')
+    countries = countries_syntax(countries)
+    return countries
 
+
+def geojson_layer(map_figure, countries_geojson, tweet_data):
+    countries = country_sentiment(tweet_data)
     colors = []
     for feature in countries_geojson['features']:
         country_name = feature['properties']['name']
@@ -85,7 +102,6 @@ def geojson_layer(map_figure, tweets, countries_geojson):
 
 def geocode_locations(tweet_data):
     geolocator = Nominatim(user_agent="plotting_points", timeout=300)
-    translator = Translator()
     lat, long, country = [], [], []
     for i in tweet_data['User Location']:
         if i == '':
@@ -93,13 +109,12 @@ def geocode_locations(tweet_data):
             long.append(None)
             country.append(None)
         else:
-            location = geolocator.geocode(i)
+            location = geolocator.geocode(i, language='en')
             if location:
                 lat.append(location.latitude)
                 long.append(location.longitude)
                 location_country = location.address.split(", ")[-1]
-                location_country_translated = translator.translate(location_country)
-                country.append(location_country_translated.text)
+                country.append(location_country)
             else:
                 lat.append(None)
                 long.append(None)
@@ -115,8 +130,8 @@ def heatmap_layer(map_figure, tweets):
     map_figure.add_layer(heatmap)
 
 
-def create_clusters(locations):
-    kmeans = KMeans(n_clusters=35, init='k-means++', random_state=10, max_iter=200)
+def create_clusters(locations, number_clusters):
+    kmeans = KMeans(n_clusters=number_clusters, init='k-means++', random_state=10, max_iter=200)
     y_kmeans = kmeans.fit_predict(locations[['Longitude', 'Latitude']])
     locations['cluster'] = y_kmeans
     avg_sentiment, Lat, Long, cluster_sizes = [], [], [], []
@@ -141,8 +156,8 @@ def create_clusters(locations):
     return clusters
 
 
-def cluster_map(map_fig, tweets):
-    clusters = create_clusters(tweets)
+def cluster_map(map_fig, tweets, number_clusters):
+    clusters = create_clusters(tweets, number_clusters)
     sentiment_color = [calculate_color(color) for color in clusters['Sentiment']]
     scales = clusters['Size'].tolist()
     minmax_scale(scales)
@@ -173,6 +188,17 @@ def cluster_map(map_fig, tweets):
     map_fig.add_layer(cluster_layer)
 
 
+def tweet_prep(tweet_data):
+    lat_long = geocode_locations(tweet_data)
+    tweet_data['Latitude'] = lat_long[0]
+    tweet_data['Longitude'] = lat_long[1]
+    tweet_data['Country'] = lat_long[2]
+    tweet_data.dropna(axis=0, inplace=True)
+    tweet_data = tweet_data.reset_index()
+
+    return tweet_data
+
+
 figure_layout = {
     'width': '100%',
     'height': '750px',
@@ -185,15 +211,13 @@ countries_geojson = gmaps.geojson_geometries.load_geometry('countries-high-resol
 
 #tweet_data = pd.read_csv('tweet_data.csv')
 tweet_data = Tweets.main()
-lat_long = geocode_locations(tweet_data)
-tweet_data['Latitude'] = lat_long[0]
-tweet_data['Longitude'] = lat_long[1]
-tweet_data['Country'] = lat_long[2]
-tweet_data.dropna(axis=0, inplace=True)
-tweet_data = tweet_data.reset_index()
+tweet_data = tweet_prep(tweet_data)
 tweet_data.to_csv('tweet_data.csv')
-scatter_plot(m, tweet_data)
-#cluster_map(m, tweet_data)
-#geojson_layer(m, tweet_data, countries_geojson)
+
+
+
+geojson_layer(m, countries_geojson, tweet_data)
+#cluster_map(m, tweet_data, int(math.sqrt(len(tweet_data.index))))
+#scatter_plot(m, tweet_data)
 
 embed_minimal_html('export.html', views=[m])
