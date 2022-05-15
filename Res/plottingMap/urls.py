4fb81f9bd7f162a6229f09daf7babac506df656d
liaolihao <3144743728@qq.com>
2022-05-15 18:39:14 +0100

    menu

--- Res/plottingMap/urls.py
+++ Res/plottingMap/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('map/', views.plotting_points),
     path('sentiment/', views.sentiment_graph),
+    path('cluster/', views.cluster_map),
+    path('geojson/', views.geo_json)
 
 ]
